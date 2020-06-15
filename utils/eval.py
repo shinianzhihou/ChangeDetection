eb6fdead9ba5150f5b3cc24e981c98e6e637f18c
Larry Zheng <46273741+Larry-Zheng@users.noreply.github.com>
2020-06-15 20:59:18 +0800

    Update eval.py

--- utils/eval.py
+++ utils/eval.py
@@ -5,6 +5,8 @@ import torchvision
 import matplotlib.image as mpimg
 
 from .metric import get_metric
+from torch.nn.functional import  interpolate 
+from tqdm import tqdm
 
 
 def eval_model(
@@ -24,13 +26,36 @@ def eval_model(
     ))
     with torch.no_grad():
         loss_all = 0.0
-        for batch, data in enumerate(data_loader):
+        
+        bar_format = '{desc}{percentage:3.0f}%|[{elapsed}<{remaining},{rate_fmt}]'
+        pbar = tqdm(data_loader,bar_format=bar_format)
+
+        for batch, data in enumerate(pbar):
             img1, img2, gt = [img.to(device) for img in data]
             output = model(img1, img2)
+            ###########解决skip connection报错问题#######
+            if output.size(-1)!=gt.size(-1) or output.size(-2)!=gt.size(-2):
+                output = interpolate(output,size=(gt.size(-2),gt.size(-1)))
+            ###########################
             if criterion:
                 loss = criterion(output,gt)
                 loss_all += loss.item()
+            
+
+                print_str = " | ".join([
+                "evaling:",
+                "batch:%3d/%3d" % (batch, data_loader.__len__()),
+                "loss:%.3f" % (loss.item()),
+                ])
+                pbar.set_description(print_str, refresh=False)
+            # ############
+            # out_tensor = output[:,1]
+            # out_tensor = (out_tensor - out_tensor.min())/(out_tensor.max() - out_tensor.min())
+
+            # ################
+
             out_tensor = torch.argmax(output, dim=1, keepdim=True).type_as(output)
+            
             gt_tensor = gt[:, 1, :, :]
             metric = get_metric(out_tensor, gt_tensor)
             metric_all = add_metric(metric_all,metric)
